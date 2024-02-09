0c46db8dfd90c354a180edb0664216374801cc7a
m-tsoi <mjtsoi@uci.edu>
2024-02-09 14:08:52 -0800

    Very Minor UI Changes
    - Changed allignment for Login, Register, and Select Plan Pages
    - Added in Diet and Nutrition XML pages
    
    - Did not add functionality yet sorry

---
+++ app/src/main/java/com/cs125/anappleaday/ui/NutritionActivity.kt
@@ -0,0 +1,15 @@
+package com.cs125.anappleaday.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.cs125.anappleaday.R
+
+class NutritionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
