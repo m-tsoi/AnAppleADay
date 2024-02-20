93985fe27c96e5b038f01f1ff8d384d9891722d9
minhngo3818 <minhngo2814@gmail.com>
2024-02-20 01:26:33 -0800

    update gradle and google services config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.cs125.anappleaday"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
