38e406c4e5677b792e62ae19792d36a11b636045
minhngo3818 <minhngo2814@gmail.com>
2024-02-23 09:39:54 -0800

    change profile dto and change Nutrition Data to DietData

--- app/src/main/java/com/cs125/anappleaday/data/sql/dto/ProfileDto.kt
+++ app/src/main/java/com/cs125/anappleaday/data/sql/dto/ProfileDto.kt
@@ -3,9 +3,9 @@ package com.cs125.anappleaday.data.sql.dto
 import com.cs125.anappleaday.data.enumTypes.Gender
 
 data class ProfileDto(
-    var name: String,
+    var name: String?,
 
-    var avatar: String,             // image file path
+    var avatar: String?,             // image file path
 
     var age: Int,
 
