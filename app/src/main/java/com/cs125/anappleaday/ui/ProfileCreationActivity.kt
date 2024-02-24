074ad2ca8d87d54aec626187bd45afc46f5eccf7
minhngo3818 <minhngo2814@gmail.com>
2024-02-23 19:11:07 -0800

    link  profile creation, add edit select plan ui

--- app/src/main/java/com/cs125/anappleaday/ui/ProfileCreationActivity.kt
+++ app/src/main/java/com/cs125/anappleaday/ui/ProfileCreationActivity.kt
@@ -2,6 +2,8 @@ package com.cs125.anappleaday.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.cs125.anappleaday.R
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
@@ -14,7 +16,15 @@ class ProfileCreationActivity: AppCompatActivity() {
 
         genderAutoCompleteText = findViewById(R.id.selectTextGender)
 
-        val sendIntent = Intent(this, SelectPlanActivity::class.java)
-        startActivity(sendIntent)
+
+        findViewById<Button>(R.id.register_button)
+            .setOnClickListener{
+                Log.d("BUTTONS", "User tapped the login")
+                //TODO: send profile info to backend
+
+                finish()
+                val sendIntent = Intent(this, SelectPlanActivity::class.java)
+                startActivity(sendIntent)
+            }
     }
 }
