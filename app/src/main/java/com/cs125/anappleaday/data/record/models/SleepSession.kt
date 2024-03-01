a5b9c5a6d2e7610ecf0d7f008a10613e722bf1d1
Khoi Pham <phamkhoiedison@gmail.com>
2024-02-29 23:18:07 -0800

    Sleep page and backend can now update each other with data

--- app/src/main/java/com/cs125/anappleaday/data/record/models/SleepSession.kt
+++ app/src/main/java/com/cs125/anappleaday/data/record/models/SleepSession.kt
@@ -3,9 +3,9 @@ package com.cs125.anappleaday.data.record.models
 import java.util.Date
 
 data class SleepSession(
-    val id: String,
-    var startTime: Long,
-    var endTime: Long,
-    var recomEndTime: Long,
-    var date: Date
+    var startTime: Date? = Date(),
+    var endTime: Date? = Date(),
+    var sleepScore: Int? = 0,
+    var recomEndTime: Date? = Date(),
+    var enteredDate: Date? = Date()
 )
