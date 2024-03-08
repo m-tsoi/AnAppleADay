f70571233823fa1294e450d0de5eb0da6e7584f3
minhngo3818 <minhngo2814@gmail.com>
2024-03-07 18:59:50 -0800

    update gradle and manifest file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
 
     // API tool
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -82,6 +83,7 @@ dependencies {
 
     // Health data api
     implementation("com.google.android.libraries.healthdata:health-data-api:1.0.0-alpha01")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
