d28fa1a2b1828925a0af7876a17e669c335db2cc
minhngo3818 <minhngo2814@gmail.com>
2024-03-08 10:29:07 -0800

    add medical record ui

---
+++ app/src/main/java/com/cs125/anappleaday/ui/MedicalRecordActivity.kt
@@ -0,0 +1,10 @@
+package com.cs125.anappleaday.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MedicalRecordActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
