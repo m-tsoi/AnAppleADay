220a36a979b867341c2b9369989ae08be45a741e
minhngo3818 <minhngo2814@gmail.com>
2024-03-10 17:29:16 -0700

    add logout out (temp)

--- app/src/main/java/com/cs125/anappleaday/ui/HomeActivity.kt
+++ app/src/main/java/com/cs125/anappleaday/ui/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.cs125.anappleaday.R
 import com.cs125.anappleaday.services.auth.FBAuth
 
@@ -39,9 +40,16 @@ class HomeActivity : AppCompatActivity() {
         findViewById<Button>(R.id.change_plan_button)
             .setOnClickListener{
                 Log.d("BUTTONS", "User tapped the login")
-                fbAuth.logout()
                 val sendIntent = Intent(this, SelectPlanActivity::class.java)
                 startActivity(sendIntent)
+            }
+
+        // Logout
+        findViewById<Button>(R.id.logout_button)
+            .setOnClickListener{
+                fbAuth.logout()
+                Toast.makeText(this, "Logged out!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginActivity::class.java))
             }
     }
 
