96d27d1f7d9ac6478aae4fa68974865754a0002e
minhngo3818 <minhngo2814@gmail.com>
2024-03-14 22:29:22 -0700

    update activity and diet fb services

--- app/src/main/java/com/cs125/anappleaday/services/firestore/FbActivityServices.kt
+++ app/src/main/java/com/cs125/anappleaday/services/firestore/FbActivityServices.kt
@@ -1,9 +1,30 @@
 package com.cs125.anappleaday.services.firestore
 
+import android.util.Log
 import com.cs125.anappleaday.data.record.models.live.ActivityData
+import com.cs125.anappleaday.data.record.models.live.DietData
+import com.cs125.anappleaday.data.record.models.live.MealRecord
+import com.cs125.anappleaday.utils.toMap
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
 
 class FbActivityServices (firestore: FirebaseFirestore) : FbBaseServices<ActivityData>(
     "ActivityData", firestore) {
-    // Note: add functions if needed
+    suspend fun getActivityData(id: String): ActivityData? {
+        return try {
+            val document = super.getDocument(id).await()
+            document.toObject<ActivityData>()
+        } catch (e: Exception) {
+            Log.e(TAG + "Activity", "${e.message}")
+            null
+        }
+    }
+
+//    fun addExerciseRecord(id: String, exercise: HashMap<String, Any>): Task<Void> {
+//        return collectionRef.document(id)
+//            .update("exercises", FieldValue.arrayUnion(exercise))
+//    }
 }
--- app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
+++ app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
@@ -1,9 +1,33 @@
 package com.cs125.anappleaday.services.firestore
 
+import android.util.Log
 import com.cs125.anappleaday.data.record.models.live.DietData
+import com.cs125.anappleaday.data.record.models.live.MealRecord
+import com.cs125.anappleaday.data.record.models.user.Personicle
+import com.cs125.anappleaday.utils.toMap
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
 
 class FbDietServices(firestore: FirebaseFirestore) : FbBaseServices<DietData>(
     "NutritionData", firestore) {
     // Note: add functions if needed
+
+    suspend fun getDietData(id: String): DietData? {
+        return try {
+            val document = super.getDocument(id).await()
+            document.toObject<DietData>()
+        } catch (e: Exception) {
+            Log.e(TAG + "Diet", "${e.message}")
+            null
+        }
+    }
+
+    // Temporary function
+//    fun addMealRecord(id: String, mealRecord: MealRecord): Task<Void> {
+//        return collectionRef.document(id)
+//            .update("meals", FieldValue.arrayUnion(mealRecord.toMap()))
+//    }
 }
