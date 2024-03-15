959728ed7d34988a980168f523f1796bf3575fc2
Carm Hermosilla <cymosilla@gmail.com>
2024-03-15 02:17:17 -0700

    Removed ExerciseRecordActivity.kt

--- app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
+++ app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
@@ -4,8 +4,8 @@ import android.health.connect.datatypes.ExerciseSessionRecord
 
 data class ActivityData(
     val id: String,
-
-    val scores: MutableList<ScoreDay>,
-
+    val scores: MutableList<ScoreDay>, // Might not be used
     val activities: MutableList<ExerciseSessionRecord>,
+    val dailyExercise
+
 )
--- app/src/main/java/com/cs125/anappleaday/ui/ExerciseViewActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.cs125.anappleaday.ui
-
-class ExerciseViewActivity {
-    // Exists to view exercise activity over time
-}
