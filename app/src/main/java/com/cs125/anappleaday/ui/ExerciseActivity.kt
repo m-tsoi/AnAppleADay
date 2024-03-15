41a7232372ef92e21b4c2afaac0904bfd47a9201
Carm Hermosilla <cymosilla@gmail.com>
2024-03-14 21:38:30 -0700

    onStart

--- app/src/main/java/com/cs125/anappleaday/ui/ExerciseActivity.kt
+++ app/src/main/java/com/cs125/anappleaday/ui/ExerciseActivity.kt
@@ -19,20 +19,28 @@ import retrofit2.Response
 import kotlin.math.roundToInt
 class ExerciseActivity : AppCompatActivity() {
 
+    // Exists to do later calculations for health
+
     private lateinit var MET: TextView // Metabolic Equivalent of Task
-    private lateinit var calories_burned: TextView // Calories burned
-    private lateinit var calories_total: TextView // Calorie total from yesterday (figure out var)
+    private lateinit var calories_burned: TextView
     private lateinit var exercise_duration: TextView
+
+    //Placeholders until retrieving stored data
+    private lateinit var calories_total: TextView // calories_burned +/- calories_total + diet
     private lateinit var weight: TextView //Placeholder for how I would obtain weight from personicle
+
+    private lateinit var recyclerRecommendations : RecyclerView //Helps recycle data already loaded, exercise data
+
+    private lateinit var submit_calories : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_exercise)
 
         MET = findViewById(R.id.MET)
+        calories_burned = findViewById(R.id.calories_burned)
         weight = findViewById(R.id.weight)
         exercise_duration = findViewById(R.id.exercise_duration)
-        calories_burned = findViewById(R.id.calories_burned)
 
         // MET, weight, and exercise duration
         // toDoubleOrNull is like ternary statement in JavaScript, return double if double, return null if nondouble
@@ -63,6 +71,7 @@ class ExerciseActivity : AppCompatActivity() {
         })
     }
 
+    // Might be scrapped due to API
     private fun caloriesBurnedCalculation(met: Double, weight: Double, durationHours: Double): Double {
         return met * weight * durationHours
     }
---
+++ app/src/main/java/com/cs125/anappleaday/ui/ExerciseViewActivity.kt
@@ -0,0 +1,5 @@
+package com.cs125.anappleaday.ui
+
+class ExerciseViewActivity {
+    // Exists to view exercise activity over time
+}
