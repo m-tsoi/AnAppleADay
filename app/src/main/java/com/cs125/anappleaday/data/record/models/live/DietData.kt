3a970ef7761616288ead76126b8c48488f4c4971
minhngo3818 <minhngo2814@gmail.com>
2024-03-15 10:14:18 -0700

    update DietServices

--- app/src/main/java/com/cs125/anappleaday/data/record/models/live/DietData.kt
+++ app/src/main/java/com/cs125/anappleaday/data/record/models/live/DietData.kt
@@ -3,8 +3,6 @@ package com.cs125.anappleaday.data.record.models.live
 import android.health.connect.datatypes.NutritionRecord
 
 data class DietData(
-    val id: String,
-
     val scores: MutableList<ScoreDay>,
 
     val nutrition: MutableList<NutritionRecord>
--- app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
+++ app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
@@ -1,9 +1,53 @@
 package com.cs125.anappleaday.services.firestore
 
+import android.health.connect.datatypes.NutritionRecord
+import android.util.Log
 import com.cs125.anappleaday.data.record.models.live.DietData
+import com.cs125.anappleaday.utils.toMap
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
 
 class FbDietServices(firestore: FirebaseFirestore) : FbBaseServices<DietData>(
     "NutritionData", firestore) {
     // Note: add functions if needed
+
+    private val subCollectionName = "NutritionRecord"
+
+    suspend fun getDietData(id: String): DietData? {
+        return try {
+            val document = super.getDocument(id).await()
+            document.toObject<DietData>()
+        } catch (e: Exception) {
+            Log.e(TAG + "HealthPlan", "${e.message}")
+            null
+        }
+    }
+
+    fun addNutritionRecord(id: String, nutritionRecord: NutritionRecord): Task<DocumentReference> {
+        var nutritionRecordMap = nutritionRecord.toMap().toMutableMap()
+        nutritionRecordMap["startTime"] = FieldValue.serverTimestamp()
+        return collectionRef.document(id).collection(subCollectionName).add(nutritionRecordMap)
+    }
+
+    suspend fun getPastNutritionRecord(id: String): MutableList<NutritionRecord> {
+        return try {
+            val documents = collectionRef.document(id).collection(subCollectionName).whereLessThan("date",
+                FieldValue.serverTimestamp()).get().await()
+
+            var nutritionRecords = mutableListOf<NutritionRecord>()
+            for (document in documents) {
+                var record = document.toObject<NutritionRecord>()
+                nutritionRecords.add(record)
+            }
+            nutritionRecords
+        } catch (e: Exception) {
+            Log.e(TAG + "HealthPlan", "${e.message}")
+            mutableListOf<NutritionRecord>()
+        }
+
+    }
 }
