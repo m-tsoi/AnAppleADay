23495f9065b046e7eb2bf5e2ff435842d7a06c73
minhngo3818 <minhngo2814@gmail.com>
2024-03-15 19:43:10 -0700

    add mealType, recommended meal model, update activity data

--- app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
+++ app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
@@ -3,8 +3,6 @@ package com.cs125.anappleaday.data.record.models.live
 import android.health.connect.datatypes.ExerciseSessionRecord
 
 data class ActivityData(
-    val id: String,
-
     val scores: MutableList<ScoreDay>,
 
     val activities: MutableList<ExerciseSessionRecord>,
