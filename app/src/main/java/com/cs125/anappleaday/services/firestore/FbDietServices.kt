54c65ff4115f993f06c8c3a7336185f1bf7ceb39
minhngo3818 <minhngo2814@gmail.com>
2024-03-15 19:45:13 -0700

    update diet and activity fb services

--- app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
+++ app/src/main/java/com/cs125/anappleaday/services/firestore/FbDietServices.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import com.cs125.anappleaday.data.record.models.live.DietData
 import com.cs125.anappleaday.utils.toMap
 import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.AggregateField
+import com.google.firebase.firestore.AggregateSource
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
@@ -12,7 +14,7 @@ import com.google.firebase.firestore.toObject
 import kotlinx.coroutines.tasks.await
 
 class FbDietServices(firestore: FirebaseFirestore) : FbBaseServices<DietData>(
-    "NutritionData", firestore) {
+    "DietData", firestore) {
     // Note: add functions if needed
 
     private val subCollectionName = "NutritionRecord"
@@ -22,7 +24,43 @@ class FbDietServices(firestore: FirebaseFirestore) : FbBaseServices<DietData>(
             val document = super.getDocument(id).await()
             document.toObject<DietData>()
         } catch (e: Exception) {
-            Log.e(TAG + "HealthPlan", "${e.message}")
+            Log.e(TAG + "Nutrition Record", "${e.message}")
+            null
+        }
+    }
+
+    suspend fun getPastNutritionRecord(id: String): MutableList<NutritionRecord> {
+        return try {
+            val documents = collectionRef.document(id).collection(subCollectionName).whereLessThan("date",
+                FieldValue.serverTimestamp()).get().await()
+
+            val nutritionRecords = mutableListOf<NutritionRecord>()
+            for (document in documents) {
+                val record = document.toObject<NutritionRecord>()
+                nutritionRecords.add(record)
+            }
+            nutritionRecords
+        } catch (e: Exception) {
+            Log.e(TAG + "Nutrition Records", "${e.message}")
+            mutableListOf<NutritionRecord>()
+        }
+    }
+
+    suspend fun getAverageScore(id: String): Double? {
+        return try {
+            val avgResult = collectionRef.document(id)
+                .collection("Scores")
+                .whereLessThan("date", FieldValue.serverTimestamp())
+                .aggregate(AggregateField.average("score"))
+                .get(AggregateSource.SERVER)
+                .await()
+
+            avgResult?.let {
+                val averageScore = it.getDouble(AggregateField.average("score")) // Extracting the average score
+                averageScore
+            }
+        } catch (e: Exception) {
+            Log.e(TAG + "Diet average score", "${e.message}")
             null
         }
     }
@@ -33,21 +71,12 @@ class FbDietServices(firestore: FirebaseFirestore) : FbBaseServices<DietData>(
         return collectionRef.document(id).collection(subCollectionName).add(nutritionRecordMap)
     }
 
-    suspend fun getPastNutritionRecord(id: String): MutableList<NutritionRecord> {
-        return try {
-            val documents = collectionRef.document(id).collection(subCollectionName).whereLessThan("date",
-                FieldValue.serverTimestamp()).get().await()
-
-            var nutritionRecords = mutableListOf<NutritionRecord>()
-            for (document in documents) {
-                var record = document.toObject<NutritionRecord>()
-                nutritionRecords.add(record)
-            }
-            nutritionRecords
-        } catch (e: Exception) {
-            Log.e(TAG + "HealthPlan", "${e.message}")
-            mutableListOf<NutritionRecord>()
-        }
-
+    fun addScoreRecord(id: String, score: Double): Task<DocumentReference> {
+         return collectionRef.document(id).collection("Scores").add(
+             hashMapOf(
+                 "score" to score,
+                 "date" to FieldValue.serverTimestamp()
+             )
+         )
     }
 }
