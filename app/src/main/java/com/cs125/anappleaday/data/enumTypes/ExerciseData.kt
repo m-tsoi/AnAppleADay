d4ff34e1042a24f6aa101edd34aad2edf1630a4b
Carm Hermosilla <cymosilla@gmail.com>
2024-03-16 20:45:20 -0700

    Change exerciseData

--- app/src/main/java/com/cs125/anappleaday/data/enumTypes/ExerciseData.kt
+++ app/src/main/java/com/cs125/anappleaday/data/enumTypes/ExerciseData.kt
@@ -1,9 +1,9 @@
 package com.cs125.anappleaday.data.enumTypes
 
-data class ExerciseData {
+data class ExerciseData (
     val name: String,
     val calories_per_hour: Int,
     val duration_minutes: Double,
     val total_calories: Int,
 
-}
+    )
