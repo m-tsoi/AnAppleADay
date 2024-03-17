f9350afb086cc93e6f35355817023b6b4817b1d4
Carm Hermosilla <cymosilla@gmail.com>
2024-03-16 21:05:27 -0700

    Minor fix to import

--- app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
+++ app/src/main/java/com/cs125/anappleaday/data/record/models/live/ActivityData.kt
@@ -4,7 +4,7 @@ import android.health.connect.datatypes.ExerciseSessionRecord
 
 data class ActivityData(
     // Similar to SleepData.kt, retrieves data from user input + recs
-    //val id: String,
+    val id: String,
     //val scores: MutableList<ScoreDay>, // Might not be used
     //val activities: MutableList<ExerciseSessionRecord>,
     val dailyExerciseRecords: MutableList<ExerciseSession> = mutableListOf<ExerciseSession>(),
--- app/src/main/java/com/cs125/anappleaday/data/recycler/ExerciseResultAdapter.kt
+++ app/src/main/java/com/cs125/anappleaday/data/recycler/ExerciseResultAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.cs125.anappleaday.data.enumTypes.ActivityData
+import com.cs125.anappleaday.data.enumTypes.ExerciseData
 
 
 class ExerciseResultAdapter(private var dataSet: MutableList<NutritionData>, private val onAddClickListener: OnAddClickListener) :
